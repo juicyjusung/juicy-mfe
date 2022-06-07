580c5887eb97d267aabdfa212ad51290fc9a48b1
juicyjusung <juicyjusung@gmail.com>
2022-06-07 22:09:42 +0900

    :construction_worker: add vercel affected deploy script

---
+++ tools/vercel-deploy/vercel-affected-deploy.sh
@@ -0,0 +1,31 @@
+
+if [ "$#" -eq 0 ]
+then
+  echo "(Need parameter)"
+    exit 1
+fi
+
+# Name of the app to check. Change this to your application name!
+APP=$1
+
+# Determine version of Nx installed
+NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
+
+# Install @nrwl/workspace in order to run the affected command
+pnpm add -D @nrwl/workspace@$NX_VERSION --prefer-offline
+pnpm add -D typescript@$TS_VERSION --prefer-offline
+
+# Run the affected command, comparing latest commit to the one before that
+npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+
+# Store result of the previous command (grep)
+IS_AFFECTED=$?
+
+if [ $IS_AFFECTED -eq 1 ]; then
+  echo "🛑 - Build cancelled"
+  exit 0
+elif [ $IS_AFFECTED -eq 0 ]; then
+  echo "✅ - Build can proceed"
+  exit 1
+fi
