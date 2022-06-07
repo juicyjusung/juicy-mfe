1ee173804aa6639eaa0145e1d3070f85beb458f8
juicyjusung <juicyjusung@gmail.com>
2022-06-07 23:49:27 +0900

    :rotating_light: remove unused codes

--- apps/header/src/app/app.tsx
+++ apps/header/src/app/app.tsx
@@ -57,39 +57,6 @@ export function App({ auth, label = '' }: Props) {
               app2
             </JButton>
           </Stack>
-
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
         </Toolbar>
       </AppBar>
     </Box>
